da8ec2445bfb4d4be2ec7bd0ae01edb4e7a1100f
YarleiWillersDev <yarleiwillers@gmail.com>
2026-03-27 16:46:54 -0300

    Commit inicial com projeto criados

---
+++ FitnessApi.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace FitnessApi.IntegrationTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ FitnessApi.UnitTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace FitnessApi.UnitTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
